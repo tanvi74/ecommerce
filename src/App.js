69e2ffc379668dc43106bf7150e1e97527f0cdb3
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-02-21 04:36:22 +0530

    CategoryPage added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import LoginPage from "./components/LogInForm/LogInForm";
 import SignupPage from "./components/SignUpForm/SignUpForm";
-
+import CategorySelect from './components/CategorySelect'
 function App() {
   return (
     <div className="App">
@@ -16,6 +16,7 @@ function App() {
           </Route>
           <Route path="/login" component={LoginPage } />
           <Route path="/signup" component={SignupPage} />
+          <Route path="/CategorySelect" component={CategorySelect} />
         </Switch>
       </Router>
     </div>
