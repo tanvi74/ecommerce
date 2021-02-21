0a57b179c5e5d3672eaef2f68d3662e3e189ec70
Ankit Jain <akjain6067.aj@gmail.com>
2021-02-21 11:34:00 +0530

    implemented API in signUp form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Header from "./components/Header/Header";
 import LoginPage from "./components/LogInForm/LogInForm";
 import SignupPage from "./components/SignUpForm/SignUpForm";
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Header />
+        <Header />
         <Switch>
           <Route path="/" exact>
             <h1>Home</h1>
           </Route>
-          <Route path="/login" component={LoginPage } />
+          <Route path="/login" component={LoginPage} />
           <Route path="/signup" component={SignupPage} />
         </Switch>
       </Router>
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,118 +1,89 @@
-import React from 'react';
-import { Form, Input, Button } from 'antd';
-import { bool } from 'prop-types';
-import defaultConfigs from './defaultConfigs';
-import './style.css';
+import React from "react";
+import { Form, Input, Button } from "antd";
+import axios from "axios";
+import defaultConfigs from "./defaultConfigs";
+import "./style.css";
 
 export default class SignUpForm extends React.Component {
-	static defaultProps = { ...defaultConfigs };
+  static defaultProps = { ...defaultConfigs };
 
-	static propTypes = {
-		/** Whether form has email input or not */
-		hasEmailInput: bool,
+  state = {
+    phone: "",
+    uid: "",
+  };
 
-		/** Whether form has phone input or not */
-		hasPhoneInput: bool,
-	};
+  onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(
+      "https://humaps-12.herokuapp.com/register_customer",
+      {
+        UID: this.state.uid,
+        PHONE: this.state.phone,
+      }
+    );
+    console.log(response);
+    window.alert(response.data.message);
+    window.location.pathname = "/";
+  };
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className="form-container">
-					<div className="top-div">
-						<h3>Please SignUp Here!</h3>
-					</div>
-					<Form onSubmitCapture={() => console.log('Form Submitted')}>
-						<div className="middle-div">
-							<Form.Item
-								label="First Name"
-								name="firstname"
-								rules={[
-									{
-										required: true,
-										message: 'Please input your Name!',
-									},
-								]}
-							>
-								<Input
-									type="text"
-									onChange={() => console.log('changed')}
-								/>
-							</Form.Item>
-							<Form.Item
-								label="Last Name"
-								name="lastname"
-								rules={[
-									{
-										required: true,
-										message: 'Please input your Name!',
-									},
-								]}
-							>
-								<Input
-									type="text"
-									onChange={() => console.log('changed')}
-								/>
-							</Form.Item>
-							{this.props.hasEmailInput && (
-								<Form.Item
-									label="Email"
-									name="email"
-									rules={[
-										{
-											required: true,
-											message: 'Please input your Email!',
-										},
-									]}
-								>
-									<Input
-										type="text"
-										onChange={() => console.log('changed')}
-									/>
-								</Form.Item>
-							)}{' '}
-							{this.props.hasPhoneInput && (
-								<Form.Item
-									label="Phone"
-									name="phone"
-									rules={[
-										{
-											required: true,
-											message: 'Please input your Phone!',
-										},
-									]}
-								>
-									<Input
-										type="number"
-										onChange={() => console.log('changed')}
-									/>
-								</Form.Item>
-							)}
-							<Form.Item
-								label="Password"
-								name="password"
-								rules={[
-									{
-										required: true,
-										message: 'Please input Password!',
-									},
-								]}
-							>
-								<Input.Password
-									onChange={() => console.log('changed')}
-								/>
-							</Form.Item>
-						</div>
-						<div className="bottom-div">
-							<Form.Item>
-								<Button type="primary" htmlType="submit">
-									SignUp
-								</Button>
-							</Form.Item>
-						</div>
-					</Form>
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <div className="form-container">
+          <div className="top-div">
+            <h3>Please SignUp Here!</h3>
+          </div>
+          <Form
+            onSubmitCapture={(e) => {
+              this.onSubmitHandler(e);
+            }}
+          >
+            <div className="middle-div">
+              <Form.Item
+                label="Phone"
+                name="phone"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Phone!",
+                  },
+                ]}
+              >
+                <Input
+                  type="number"
+                  onChange={(e) => {
+                    this.setState({ phone: e.target.value });
+                  }}
+                />
+              </Form.Item>
+              <Form.Item
+                label="UID"
+                name="uid"
+                rules={[
+                  {
+                    required: true,
+                    message: "UID required",
+                  },
+                ]}
+              >
+                <Input
+                  type="text"
+                  onChange={(e) => {
+                    this.setState({ uid: e.target.value });
+                  }}
+                />
+              </Form.Item>
+            </div>
+            <div className="bottom-div">
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  SignUp
+                </Button>
+              </Form.Item>
+            </div>
+          </Form>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
