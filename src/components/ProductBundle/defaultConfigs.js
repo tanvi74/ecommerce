9c67a35d23551025d2e51db3fb30971adfe55b85
Ankit Jain <akjain6067.aj@gmail.com>
2021-02-21 19:38:07 +0530

    added ProductBundle component

---
+++ src/components/ProductBundle/defaultConfigs.js
@@ -0,0 +1,46 @@
+const defaultConfigs = {
+  items: [
+    {
+      size: 200,
+      productName: "Demo Product",
+      imgURL: "https://via.placeholder.com/200/771796",
+      price: 10,
+      priceCurrencySign: "$",
+      description: "Product Description",
+    },
+    {
+      size: 200,
+      productName: "Demo Product",
+      imgURL: "https://via.placeholder.com/200/771796",
+      price: 10,
+      priceCurrencySign: "$",
+      description: "Product Description",
+    },
+    {
+      size: 200,
+      productName: "Demo Product",
+      imgURL: "https://via.placeholder.com/200/771796",
+      price: 10,
+      priceCurrencySign: "$",
+      description: "Product Description",
+    },
+    {
+      size: 200,
+      productName: "Demo Product",
+      imgURL: "https://via.placeholder.com/200/771796",
+      price: 10,
+      priceCurrencySign: "$",
+      description: "Product Description",
+    },
+    {
+      size: 200,
+      productName: "Demo Product",
+      imgURL: "https://via.placeholder.com/200/771796",
+      price: 10,
+      priceCurrencySign: "$",
+      description: "Product Description",
+    },
+  ],
+};
+
+export default defaultConfigs;
