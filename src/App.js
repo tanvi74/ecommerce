c591144f17be6596f833f74ae68cba2055956760
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-02-21 20:31:43 +0530

    category select along with api integration

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import LoginPage from "./components/LogInForm/LogInForm";
 import SignupPage from "./components/SignUpForm/SignUpForm";
-import CategorySelect from './components/CategorySelect'
+import CategorySelect from './components/CategorySelect';
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
           <Route path="/login" component={LoginPage } />
           <Route path="/signup" component={SignupPage} />
           <Route path="/CategorySelect" component={CategorySelect} />
+          
         </Switch>
       </Router>
     </div>
--- src/components/CategorySelect.js
+++ src/components/CategorySelect.js
@@ -1,35 +1,61 @@
+// this page will be visible weather logged in or not
+
 import React from 'react';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css'; //antd library is used
 import { Menu , Button} from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
-import {
-
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
-} from '@ant-design/icons';
-
+import { MailOutlined, } from '@ant-design/icons';
+import { Input, Space } from 'antd';
 const { SubMenu } = Menu;
+const { Search } = Input;
 
-class CategorySelect extends React.Component {
+class CategorySelect extends React.Component {  
+
+  //class based component created where a user can see the list of all items and if he enters the pincode the items will be filtered according to that
   state = {
+    items:[],
     collapsed: false,
   };
 
+
+ onSearch = async (value)=> {console.log(value);
+try{
+  let _data = {
+    pincode: value,
+    categoryId: "60053042c41535052c98e6ae", 
+    subCategoryId:"123"
+
+  }
+  const url="https://humaps-12.herokuapp.com/get_products";
+  const response = await fetch(url,{
+    method: "POST",
+    body: JSON.stringify(_data),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+  });
+  const data = await response.json();
+  console.log(data)
+
+this.setState(
+  {
+    items:data.data
+  }
+)
+}
+catch(err)
+{
+  console.log("error!");
+}
+}
   toggleCollapsed = () => {
     this.setState({
       collapsed: !this.state.collapsed,
     });
   };
 
+
+
   render() {
     return (
-      <div style={{ width: 2566 }}>
-        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-          {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-        </Button>
+      <div style={{ width:" 100%", height:"10%"}}>
         <Menu
           defaultSelectedKeys={['1']}
           defaultOpenKeys={['sub1']}
@@ -37,7 +63,6 @@ class CategorySelect extends React.Component {
           theme="dark"
           inlineCollapsed={this.state.collapsed}
         >
-          
           <SubMenu key="sub1" icon={<MailOutlined />} title="Electronics">
             <Menu.Item key="5">Option 5</Menu.Item>
             <Menu.Item key="6">Option 6</Menu.Item>
@@ -63,6 +88,24 @@ class CategorySelect extends React.Component {
             <Menu.Item key="8">Option 8</Menu.Item>
           </SubMenu>
         </Menu>
+        <Search
+      placeholder="input Pincode"
+      allowClear
+      enterButton="Search"
+      size="large"
+      onSearch={this.onSearch}
+
+    />
+      {this.state.items.map(
+        (v)=>{
+          return(
+            <div>
+              {v.product_details.name}
+            </div>
+          )
+
+        }
+      )}
       </div>
     );
   }
