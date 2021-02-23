db8989e0362bf73f2e5208b7995b218554de1b12
Ankit Jain <akjain6067.aj@gmail.com>
2021-02-23 18:33:25 +0530

    integrated search_by_pincode api

--- src/App.js
+++ src/App.js
@@ -1,72 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import axios from "axios";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import LoginPage from "./components/LogInForm/LogInForm";
 import SignupPage from "./components/SignUpForm/SignUpForm";
 import CategorySelect from "./components/CategorySelect";
-class App extends React.Component {
-  state = {
-    searchedItems: [],
-    homeItems: [],
-  };
-  onSearchSubmit = async (searchTerm) => {
-    console.log("searched");
-    console.log(searchTerm);
-    // const response = await axios.post(
-    //   "https://humaps-12.herokuapp.com/allproducts",
-    //   { pincode: searchTerm }
-    // );
-    // console.log(response);
-    // this.setState({ searchedItems: response.data.data });
-  };
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
 
-  componentDidMount() {
-    console.log("from gethomeitems");
-    axios
-      .get("https://humaps-12.herokuapp.com/allproducts")
-      .then((response) => {
-        console.log(response.data.data);
-        const productData = response.data.data.map((item) => {
-          return {
-            productName: item.name,
-            price: item.MRP,
-            priceCurrencySign: "₹",
-          };
-        });
-        console.log("Productdata", productData);
-        this.setState({ homeItems: productData });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Header
-            onSearchSubmit={(searchTerm) => this.onSearchSubmit(searchTerm)}
-            items={this.state.homeItems}
-          />
-          <Switch>
-            <Route path="/" exact>
-              <Home items={this.state.homeItems} />
-            </Route>
-            <Route path="/login" component={LoginPage} />
-            <Route path="/signup" component={SignupPage} />
-            <Route path="/CategorySelect" component={CategorySelect} />
-            <Route path="/searchresults">
-              <h1>Search Results</h1>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {console.log(searchTerm)}
+      <Router>
+        <Header onSearchSubmit={(searchTerm) => setSearchTerm(searchTerm)} />
+        <Switch>
+          <Route path="/" exact>
+            <Home searchTerm={searchTerm} />
+          </Route>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignupPage} />
+          <Route path="/CategorySelect" component={CategorySelect} />
+          <Route path="/searchresults"></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ProductBundle from "./../ProductBundle/ProductBundle";
 
 import "./style.css";
 
-class Home extends React.Component {
-  state = {};
+function Home(props) {
+  const [productList, setProductList] = useState([]);
 
-  render() {
-    console.log(this.props.items);
-    return (
-      <>
-        <div className="home">
-          <h1>Welcome To HyperLocal</h1>
-          <ProductBundle items={this.props.items} />
-        </div>
-      </>
-    );
-  }
+  const getProducts = async () => {
+    var url;
+    props.searchTerm
+      ? (url = `${window.apihost}/get_products_via_pincode`)
+      : (url = `${window.apihost}/get_all_products`);
+
+    console.log("from get products");
+
+    var data = {};
+    props.searchTerm
+      ? (data = {
+          pincode: props.searchTerm,
+        })
+      : (data = {});
+
+    var resp = await axios.post(url, data);
+    console.log(resp.data.data);
+    const productList = resp.data.data.map((item) => {
+      return {
+        key: item._id,
+        productName: item.name,
+        price: item.MRP,
+        description: item.description,
+      };
+    });
+    setProductList(productList);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [props.searchTerm]);
+
+  return (
+    <>
+      <div className="home">
+        <h1>Welcome To HyperLocal</h1>
+        <ProductBundle items={productList} />
+      </div>
+    </>
+  );
 }
 
 export default Home;
