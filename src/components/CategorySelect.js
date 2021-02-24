be8f8f6a48b767c266628acf1f455f41598e88f4
Ankit Jain <akjain6067.aj@gmail.com>
2021-02-24 20:16:38 +0530

    code formatted

--- src/components/CategorySelect.js
+++ src/components/CategorySelect.js
@@ -1,63 +1,59 @@
 // this page will be visible weather logged in or not
 
-import React from 'react';
-import 'antd/dist/antd.css'; //antd library is used
-import { Menu , Button} from 'antd';
-import { MailOutlined, } from '@ant-design/icons';
-import { Input, Space } from 'antd';
+import React from "react";
+import "antd/dist/antd.css"; //antd library is used
+import { Menu } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 const { SubMenu } = Menu;
 const { Search } = Input;
 
-class CategorySelect extends React.Component {  
-
+class CategorySelect extends React.Component {
   //class based component created where a user can see the list of all items and if he enters the pincode the items will be filtered according to that
   state = {
-    items:[],
+    items: [],
     collapsed: false,
   };
 
+  onSearch = async (value) => {
+    console.log(value);
+    try {
+      let _data = {
+        pincode: value,
+        categoryId: "60053042c41535052c98e6ae",
+        subCategoryId: "123",
+      };
+      const url = "https://humaps-12.herokuapp.com/get_products";
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(_data),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      const data = await response.json();
+      console.log(data);
 
- onSearch = async (value)=> {console.log(value);
-try{
-  let _data = {
-    pincode: value,
-    categoryId: "60053042c41535052c98e6ae",
-    subCategoryId:"123"
-  }
-  const url="https://humaps-12.herokuapp.com/get_products";
-  const response = await fetch(url,{
-    method: "POST",
-    body: JSON.stringify(_data),
-    headers: {"Content-type": "application/json; charset=UTF-8"}
-  });
-  const data = await response.json();
-  console.log(data)
-
-this.setState(     //will update items
-  {
-    items:data.data
-  }
-)
-}
-catch(err)
-{
-  console.log("error!");
-}
-}
+      this.setState(
+        //will update items
+        {
+          items: data.data,
+        }
+      );
+    } catch (err) {
+      console.log("error!");
+    }
+  };
   toggleCollapsed = () => {
     this.setState({
       collapsed: !this.state.collapsed,
     });
   };
 
-
-
   render() {
     return (
-      <div style={{ width:" 100%", height:"10%"}}>
+      <div style={{ width: " 100%", height: "10%" }}>
         <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
           mode="inline"
           theme="dark"
           inlineCollapsed={this.state.collapsed}
@@ -88,23 +84,15 @@ catch(err)
           </SubMenu>
         </Menu>
         <Search
-      placeholder="input Pincode"
-      allowClear
-      enterButton="Search"
-      size="large"
-      onSearch={this.onSearch}
-
-    />
-      {this.state.items.map(
-        (v)=>{
-          return(
-            <div>
-              {v.product_details.name}
-            </div>
-          )
-
-        }
-      )}
+          placeholder="input Pincode"
+          allowClear
+          enterButton="Search"
+          size="large"
+          onSearch={this.onSearch}
+        />
+        {this.state.items.map((v) => {
+          return <div>{v.product_details.name}</div>;
+        })}
       </div>
     );
   }
