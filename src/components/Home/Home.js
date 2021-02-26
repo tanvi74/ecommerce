68c3ced47b5e9d812b3528ed556f75d154b8baf9
Ankit Jain <akjain6067.aj@gmail.com>
2021-02-26 15:21:37 +0530

    showing loader when fetching data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {useSelector} from 'react-redux';
-import {Row,Col,Button, Dropdown, Menu} from 'antd';
+import { useSelector } from "react-redux";
+import { Row, Col, Button, Dropdown, Menu, Spin } from "antd";
 import ProductBundle from "./../ProductBundle/ProductBundle";
 
 import "./style.css";
@@ -9,7 +9,8 @@ import "./style.css";
 function Home(props) {
   const [productList, setProductList] = useState([]);
   const [sortSeq, setSortSeq] = useState("highToLow");
-  const searchTerm = useSelector(state => state.searchTerm);
+  const [isLoading, setIsLoading] = useState(false);
+  const searchTerm = useSelector((state) => state.searchTerm);
   const getProducts = async () => {
     var url;
     searchTerm
@@ -25,17 +26,22 @@ function Home(props) {
         })
       : (data = {});
 
+    setIsLoading(true);
     var resp = await axios.post(url, data);
+    setIsLoading(false);
     console.log(resp.data.data);
-    const productList = resp.data.data!=null ? resp.data.data.map((item) => {
-      return {
-        key: item._id,
-        productName: item.name,
-        price: item.MRP,
-        description: item.description,
-        imgURL: item.images[0],
-      };
-    }): [];
+    const productList =
+      resp.data.data != null
+        ? resp.data.data.map((item) => {
+            return {
+              key: item._id,
+              productName: item.name,
+              price: item.MRP,
+              description: item.description,
+              imgURL: item.images[0],
+            };
+          })
+        : [];
     setProductList(productList);
   };
 
@@ -43,58 +49,61 @@ function Home(props) {
     getProducts();
   }, [searchTerm]);
 
-  function compare( a, b ) {
-    if ( a.price < b.price ){
+  function compare(a, b) {
+    if (a.price < b.price) {
       return -1;
     }
-    if ( a.price > b.price ){
+    if (a.price > b.price) {
       return 1;
     }
     return 0;
   }
 
-  function inversecompare( a, b ) {
-    if ( a.price > b.price ){
+  function inversecompare(a, b) {
+    if (a.price > b.price) {
       return -1;
     }
-    if ( a.price < b.price ){
+    if (a.price < b.price) {
       return 1;
     }
     return 0;
   }
 
-let promise = new Promise(function(resolve, reject) {
-  var arr = []
-  sortSeq === "lowToHigh" ? arr = productList.sort(compare) :  productList.sort(inversecompare); 
-  resolve(arr);
-})
- 
+  let promise = new Promise(function (resolve, reject) {
+    var arr = [];
+    sortSeq === "lowToHigh"
+      ? (arr = productList.sort(compare))
+      : productList.sort(inversecompare);
+    resolve(arr);
+  });
 
-  const lowToHigh= () => {
+  const lowToHigh = () => {
     console.log("lowToHigh");
-    setSortSeq("lowToHigh")
+    setSortSeq("lowToHigh");
     promise.then(
-      result => console.log(result), 
-      error => console.log(error) 
+      (result) => console.log(result),
+      (error) => console.log(error)
     );
-   
-  }
+  };
 
   const highToLow = () => {
     console.log("highToLow");
-    setSortSeq("highToLow")
+    setSortSeq("highToLow");
     promise.then(
-      result => console.log(result) , 
-      error => console.log(error) 
+      (result) => console.log(result),
+      (error) => console.log(error)
     );
-  }
-
+  };
 
   // menu for dropdown
   const menu = (
-    <Menu  >
-      <Menu.Item key="1" onClick={highToLow}>From High to Low</Menu.Item>
-      <Menu.Item key="2" onClick={lowToHigh}>From Low to High</Menu.Item>
+    <Menu>
+      <Menu.Item key="1" onClick={highToLow}>
+        From High to Low
+      </Menu.Item>
+      <Menu.Item key="2" onClick={lowToHigh}>
+        From Low to High
+      </Menu.Item>
     </Menu>
   );
 
@@ -102,21 +111,26 @@ let promise = new Promise(function(resolve, reject) {
     <>
       <div className="home">
         {console.log(productList)}
-        
-        
-        {productList.length ?<> 
-          <Row>
-            <Col style={{marginBottom: 100}}>
-            <Dropdown overlay={menu} placement="bottomRight" arrow>
-                <Button type="primary" size="large">Sort By Price</Button>
-            </Dropdown>
-            </Col>
-          </Row>
-        <ProductBundle items={productList} /> </>: 
-         <>
-         <h1>Sorry!! No Products in your pincode area.</h1>
-          </>}
-        
+        {isLoading ? (
+          <Spin size="large" />
+        ) : productList.length ? (
+          <h1>
+            <Row>
+              <Col style={{ marginBottom: 100 }}>
+                <Dropdown overlay={menu} placement="bottomRight" arrow>
+                  <Button type="primary" size="large">
+                    Sort By Price
+                  </Button>
+                </Dropdown>
+              </Col>
+            </Row>
+            <ProductBundle items={productList} />{" "}
+          </h1>
+        ) : (
+          <>
+            <h1>Sorry!! No Products in your pincode area.</h1>
+          </>
+        )}
       </div>
     </>
   );
